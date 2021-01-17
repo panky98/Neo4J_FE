bc77f1ac54745ef86e84a761d6dfae5e2b02059b
DuleDimitrov123 <65020818+DuleDimitrov123@users.noreply.github.com>
2021-01-17 19:11:16 +0100

    Ispravka2
    Ispravka2

--- FEReact/FE_REACT/src/components/Projekti.js
+++ FEReact/FE_REACT/src/components/Projekti.js
@@ -1,16 +1,33 @@
 import React, {useState} from 'react'
 import useFetch from '../services/useFetch.js';
 
+import { Link, NavLink } from "react-router-dom";
+
 import Spinner from '../components/Spinner.js';
 import Projekat from './Projekat.js';
 import NapraviProjekat from '../components/NapraviProjekat.js';
 
-function Projekti() {
+function Projekti(id) {
+    async function obrisiProjekat(id)
+    {
 
+        console.log("brisem " + id);
+        await fetch("https://localhost:44392/Projekat/obrisiProjekat/"+id,{
+        method:"DELETE",
+        headers:{"Content-Type":"application/json"},
+    }).then(p=>{
+        if(p.ok){
+            console.log("Uspesno obrisano!");
+        }
+    }).catch(exc=>{
+        console.log(exc);
+    });
+    window.location.reload(false);
+    }
 
     const [tipProjekta, setTipProjekta]=useState("svi");
 
-    
+ 
 
     let pathUpita="Projekat/vratiSveProjekte";
     if(tipProjekta==='gotov')
@@ -22,6 +39,8 @@ function Projekti() {
 
     if(error) throw error;
     if(loading) return <Spinner/>
+
+    
     console.log(projekti)
 
     
@@ -37,8 +56,18 @@ function Projekti() {
             </select>
             {projekti.map(p=>
             {
-                return <Projekat key={p.id} projekat={p}/>
-            })}
+                return(
+                    <div key={p.id}>
+                        <h2>{p.naziv}</h2>
+                        <p>Pocetak:{p.datum_od}</p>
+                        {(p.datum_do!=="0001-01-01T00:00:00" && p.datum_do!=="1901-01-31T23:00:00Z") ?  <p>Kraj: {p.datum_do}</p> : <p>Nije gotov</p>}
+                        <Link to={`/projekti/${p.id}`} className="btn">Saznaj vise</Link>
+                        <button onClick={()=>obrisiProjekat(p.id)}>Obrisi projekat</button>
+                    </div>
+
+                //return <Projekat key={p.id} projekat={p} firme={firme}/>
+            )})}
+        
         </div>
     )
 }
